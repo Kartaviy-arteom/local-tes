ee4e3c6b856b465b324c8e0e3552e087026617db
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2022-12-07 17:07:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const btn = document.querySelector('#button');
-const URL = 'http://192.168.0.110:3000';
+const URL = 'https://192.168.0.110:3000';
 
 const handleBtnClick = async function() {
     let response = await fetch(URL);
